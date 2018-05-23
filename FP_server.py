1bac8ca98dffc93350ac8bfc24c6ec56db3b603a
M Hendri F <hendrifebriansyah28@gmail.com>
2018-05-23 08:53:47 +0700

    update server

--- FP_server.py
+++ FP_server.py
@@ -1,10 +1,22 @@
+import Pyro4
 import hashlib
 
 def get_node(namafile):
-	petalokasi = {}
-	petalokasi ['a'] = 'PYRO:example.warehouse@localhost:51279'
-	h1= hashlib.md5(namafile).hexdigest()[-1]
-	return petalokasi[h1]
+    petalokasi = {}
+    petalokasi ['A'] = 'PYRO:example.warehouse@localhost:51279'
+    petalokasi['B'] = 'PYRO:example.warehouse@localhost:51280'
+    petalokasi['C'] = 'PYRO:example.warehouse@localhost:51281'
+    petalokasi['D'] = 'PYRO:example.warehouse@localhost:51282'
+
+    h1= hashlib.md5(namafile).hexdigest()[-1]
+    if  h1 == '0' or h1 == '1' or h1 == '2' or h1 == '3':
+        return petalokasi['A']
+    elif h1 == '4' or h1 == '5' or h1 == '6' or h1 == '7':
+        return petalokasi['B']
+    elif h1 == '8' or h1 == '9' or h1 == 'a' or h1 == 'b':
+        return petalokasi['C']
+    elif h1 == 'c' or h1 == 'd' or h1 == 'e' or h1 == 'f':
+        return petalokasi['D']
 
 	# h1= hashlib.md5(namafile).hexdigest()[-1]
 	# cek = cekkoneksi(h1)
@@ -17,10 +29,10 @@ def get_node(namafile):
 
 	# return False
 
-def storefile(namafile,isifile):
+def storefile(namafile, isifile):
 	lokasi = get_node(namafile)
 	storage = Pyro4.Proxy(lokasi)
-	storage.storefile(namafile,content)
+	storage.storefile(namafile, content)
 
 def getfile(namafile):
 	storage = Pyro4.Proxy(lokasi)
